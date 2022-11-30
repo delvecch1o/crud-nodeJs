9536ab726a4e929abcdb79113730a90a3078f977
delvecch1o <rodrigopereirapinheiro@gmail.com>
2022-11-29 22:22:41 -0300

    alterando o verbo da rota

--- back/src/routes/routes.js
+++ back/src/routes/routes.js
@@ -7,7 +7,7 @@ const User = require('../controllers/usersControllers')
 routes.post('/api/register', User.create);
 routes.get('/api/register', User.index);
 routes.get('/api/details/:_id', User.details);
-routes.patch('/api/register/:_id', User.update);
+routes.put('/api/register/:_id', User.update);
 routes.delete('/api/register/:_id', User.delete);
 
 
